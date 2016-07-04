312eeefaf969484a3772bbcf0f6b6c2ff458d863
Gilson Gabriel Martins Diniz <gilson.gabriel@academico.ifrn.edu.br>
2016-07-04 15:25:16 -0300

    Hello, world

---
+++ HelloAPP2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HelloAPP2.Resources;
+
+namespace HelloAPP2
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
